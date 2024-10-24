fbd05c49412362fa40e9141bb2d5e4da2ad4ae42
light-planck <seseifunc16@gmail.com>
2024-10-24 09:27:20 +0900

    feat: 自作middlewareを適用

--- config.ru
+++ config.ru
@@ -2,6 +2,8 @@
 
 require 'rack'
 require_relative 'app'
+require_relative 'simple_middleware'
 
 use Rack::Runtime
+use SimpleMiddleware
 run App.new
---
+++ simple_middleware.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+##
+class SimpleMiddleware
+  def initialize(app)
+    puts '*' * 50
+    puts "* #{self.class} initialize(app = #{app.class})"
+    puts '*' * 50
+
+    @app = app
+  end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+
+    puts '*' * 50
+    puts "* #{self.class} call(body = #{body})"
+    puts '*' * 50
+
+    [status, headers, body]
+  end
+end
